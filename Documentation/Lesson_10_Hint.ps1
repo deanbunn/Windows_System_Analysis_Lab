1d32c21b9ff8536ab98119a72b3e0a5371e9510d
deanbunn <deanbunn@gmail.com>
2023-06-10 17:32:44 -0700

    20230610_1732hrs PnP Extended

---
+++ Documentation/Lesson_10_Hint.ps1
@@ -0,0 +1,33 @@
+
+$arrReporting = @();
+
+$pnpDevices = Get-PnpDevice -Class AudioEndpoint,Camera -PresentOnly | Get-PnpDeviceProperty | Sort-Object InstanceId,KeyName | Where-Object -Property KeyName -in -Value "DEVPKEY_Device_FriendlyName", "DEVPKEY_Device_InstallDate" | Group-Object -Property InstanceId;
+
+foreach($pnpDvc in $pnpDevices)
+{
+    #Custom Reporting Object
+    $cstDevObj = New-Object PSObject -Property (@{id=""; name=""; install_date="";});
+
+    #Set ID
+    $cstDevObj.id = $pnpDvc.Name;
+
+    foreach($dvGM in $pnpDvc.Group)
+    {
+        #Pull Friendly Name
+        if($dvGM.KeyName -eq "DEVPKEY_Device_FriendlyName") 
+        {
+            $cstDevObj.name = $dvGM.Data;
+        }
+
+        #Pull Install Date
+        if($dvGM.KeyName -eq "DEVPKEY_Device_InstallDate") 
+        {
+            $cstDevObj.install_date = $dvGM.Data;
+        }
+
+    }
+
+    $arrReporting += $cstDevObj;
+}
+
+$arrReporting; 
