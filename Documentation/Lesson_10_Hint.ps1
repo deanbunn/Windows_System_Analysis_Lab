d78df9a9621c02aa2a450cc01ccaa45e793cc651
deanbunn <deanbunn@gmail.com>
2023-06-11 11:23:20 -0700

    20230611_1123hrs Lesson 10 Hint

--- Documentation/Lesson_10_Hint.ps1
+++ Documentation/Lesson_10_Hint.ps1
@@ -1,33 +1,102 @@
+#Stopping an Accidental Run
+exit
 
-$arrReporting = @();
+<#
+    Script: DWS_File_Server_Reports.ps1
+#>
 
-$pnpDevices = Get-PnpDevice -Class AudioEndpoint,Camera -PresentOnly | Get-PnpDeviceProperty | Sort-Object InstanceId,KeyName | Where-Object -Property KeyName -in -Value "DEVPKEY_Device_FriendlyName", "DEVPKEY_Device_InstallDate" | Group-Object -Property InstanceId;
+#Var for Server Name
+[string]$srvName = (hostname).ToString().ToUpper();
 
-foreach($pnpDvc in $pnpDevices)
+#Var for Report Date
+[string]$rptDate = (Get-Date).ToString("yyyy-MM-dd");
+
+#Reporting Array for SMB Shares
+$raSMB = @();
+
+#Reporting Array for SMB Share Access
+$raSMBAccess = @();
+
+#Reporting Array for Share Folder ACLs
+$raShareFldACLs = @();
+
+
+#Pull SMB Share Information
+foreach($fsSMB in (Get-SmbShare))
 {
-    #Custom Reporting Object
-    $cstDevObj = New-Object PSObject -Property (@{id=""; name=""; install_date="";});
+    
+    #Create Custom SMB Share Object
+    $cstSMB = new-object PSObject -Property (@{ Name=""; Description=""; Path=""; });
 
-    #Set ID
-    $cstDevObj.id = $pnpDvc.Name;
+    #Pull Report Values
+    $cstSMB.Name = $fsSMB.Name;
+    $cstSMB.Path = $fsSMB.Path;
 
-    foreach($dvGM in $pnpDvc.Group)
+    #Add SMB Share Information to Reporting Array
+    $raSMB += $cstSMB;
+
+    #Pull SMB Access Permissions
+    foreach($fsSMBAccess in (Get-SmbShareAccess -Name $fsSMB.Name))
     {
-        #Pull Friendly Name
-        if($dvGM.KeyName -eq "DEVPKEY_Device_FriendlyName") 
+
+        #Create Custom SMB Share Access Object
+        $cstSMBAccess = new-object PSObject -Property (@{ ShareName=""; SharePath=""; AccountName=""; AccessControlType=""; AccessRight=""; });
+
+        #Pull Report Values
+        $cstSMBAccess.ShareName = $fsSMBAccess.Name;
+        $cstSMBAccess.SharePath = $fsSMB.Path;
+        $cstSMBAccess.AccountName = $fsSMBAccess.AccountName;
+        $cstSMBAccess.AccessControlType = $fsSMBAccess.AccessControlType;
+        $cstSMBAccess.AccessRight = $fsSMBAccess.AccessRight;
+
+        #Add SMB Share Access information to Reporting Array
+        $raSMBAccess += $cstSMBAccess;
+
+    }#End of Get-SmbShareAccess Foreach
+
+
+    #Pull Share Folder ACLs
+    if([string]::IsNullOrEmpty($fsSMB.Path) -eq $false)
+    {
+    
+        foreach($fsACL in ((Get-Acl -Path $fsSMB.Path).Access))
         {
-            $cstDevObj.name = $dvGM.Data;
-        }
+       
+            #Create Custom Shared Folder ACL Object
+            $cstFsACL = new-object PSObject -Property (@{ ShareName=""; SharePath=""; IdentityReference=""; FileSystemRights=""; AccessControlType=""; IsInherited=""; });
 
-        #Pull Install Date
-        if($dvGM.KeyName -eq "DEVPKEY_Device_InstallDate") 
-        {
-            $cstDevObj.install_date = $dvGM.Data;
-        }
+            #Pull Report Values
+            $cstFsACL.ShareName = $fsSMB.Name;
+            $cstFsACL.SharePath = $fsSMB.Path;
+            $cstFsACL.IdentityReference = $fsACL.IdentityReference;
+            $cstFsACL.FileSystemRights = $fsACL.FileSystemRights;
+            $cstFsACL.AccessControlType = $fsACL.AccessControlType;
+            $cstFsACL.IsInherited = $fsACL.IsInherited;
 
-    }
+            #Add Custome Shared Folder ACL to Reporting Array
+            $raShareFldACLs += $cstFsACL;
 
-    $arrReporting += $cstDevObj;
+        }#End of Get-Acl on Shared Folder
+
+    }#End of Null\Empty Checks on Path
+
 }
 
-$arrReporting; 
+#Var for SMB Report Name
+[string]$rptNameSMB = ".\DWS_Report_FileServer_SMBs_on_" + $srvName + "_" + $rptDate + ".csv";
+
+#Var for SMB Access Report Name
+[string]$rptNameSMBAccess = ".\DWS_Report_FileServer_SMBAccess_on_" + $srvName + "_" + $rptDate + ".csv";
+
+#Var for Shared Folder ACL Report Name
+[string]$rptNameSFACLs = ".\DWS_Report_FileServer_SharedFolderACLs_on_" + $srvName + "_" + $rptDate + ".csv";
+
+#Export SMB Report to CSV
+$raSMB | Sort-Object -Property Path | Select-Object -Property Name,Path,Description | Export-Csv -Path $rptNameSMB -NoTypeInformation;
+
+#Export SMB Access Report to CSV
+$raSMBAccess | Sort-Object -Property SharePath | Select-Object -Property ShareName,AccountName,AccessControlType,AccessRight,SharePath | Export-Csv -Path $rptNameSMBAccess -NoTypeInformation;
+
+#Export Shared Folder ACLs Report to CSV
+$raShareFldACLs | Sort-Object -Property SharePath | Select-Object -Property ShareName,IdentityReference,FileSystemRights,AccessControlType,IsInherited,SharePath | Export-Csv -Path $rptNameSFACLs -NoTypeInformation;
+
