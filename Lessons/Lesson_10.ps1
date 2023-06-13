ea6ffd2e18226c01456c8216b7b711c82f8a6f49
deanbunn <deanbunn@gmail.com>
2023-06-12 21:30:49 -0700

    20230612_2130hrs Lesson 10 Loops

--- Lessons/Lesson_10.ps1
+++ Lessons/Lesson_10.ps1
@@ -1,21 +1,41 @@
 <#
     Script: Lesson_10.ps1
-    Last Modified: 2023-06-10
+    Last Modified: 2023-06-12
 #>
 
 #Stopping an Accidental Run
-exit
+#exit
 
-#Write a Script to Report the File Permissions of a Program File Location
+#Write a Script to Report the File Permissions of all Program Files Folders ()
 
 
 #ProgramFiles                   C:\Program Files
 #ProgramFiles(x86)              C:\Program Files (x86)
 #windir                         C:\WINDOWS
-#Get-Process -FileVersionInfo -ErrorAction "SilentlyContinue" | Select-Object OriginalFilename,FileVersionRaw,FileName | Sort-Object OriginalFilename
 
-#Get All processes currently running and check the "FileName" property for any paths that fall into the $env:programfiles or $env:programfiles(x86) or $env:windir directories. 
 
-Get-Process -FileVersionInfo -ErrorAction "SilentlyContinue" | Format-List
+#Array of Locations to Check
+$arrLocsToCheck = @(${env:programfiles(x86)},${env:programfiles},${env:windir});
 
-foreach($d in $dean){ write-output $d.OriginalFileName; write-output $d.FileName;   }
+#Loop Through the Locations to Check
+foreach($LocToCheck in $arrLocsToCheck)
+{
+    #Pull Directories Under the Locations to Check
+    foreach($ltcFldr in (Get-ChildItem -Path $LocToCheck -Directory -Depth 0))
+    {
+       
+        foreach($fsACL in (Get-Acl -Path $ltcFldr.FullName).Access)
+        {
+            if($fsACL.IsInherited -eq $false)
+            {
+                $fsACL.IdentityReference;
+                #$fsACL.FileSystemRights;
+                #$fsACL.AccessControlType;
+                #$fsACL.IsInherited;
+            }
+           
+        }
+
+    }
+
+}
